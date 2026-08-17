8666fb0c6293285d8097989c1624593f67f72374
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2026-08-17 16:43:48 +0530

    changes updated for the files

--- Hooks/useState/src/App.tsx
+++ Hooks/useState/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div style={{textAlign: "center"}}>
       <h1>
         My primary color is <span style={{color: color}}>{color}</span>
       </h1>
